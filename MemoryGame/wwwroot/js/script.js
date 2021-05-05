c9e43122f48cff8cb01b27e72e1b810eed4967d9
DanielBonesDimov DanielBonesDimov <danieldimov10g@abv.bg>
2021-05-05 16:53:27 +0300

    Super

--- MemoryGame/wwwroot/js/script.js
+++ MemoryGame/wwwroot/js/script.js
@@ -1,70 +1,196 @@
-﻿const cards = document.querySelectorAll('.memory-card');
+﻿const cards = [ "Luigi.jpg", "Luigi.jpg", "Walugi.jpg", "Walugi.jpg","Toad.jpg", "Toad.jpg", "Yoshi.jpg", "Yoshi.jpg","Mario.jpg","Mario.jpg","Goomba.jpg","Goomba.jpg"]
+const cardsContainer = document.querySelector(".memory-game");
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+let matchedCards = [];
+let currentOpenedCards = [];
+let moves = 0;
+let time;
+let starCount = 3;
+let minutes = 0;
+let seconds = 0;
+  let  timeStart = false;
+const modal = document.getElementById("modal");
+const timeCounter = document.querySelector(".timer");
+const reset = document.querySelector(".reset-btn");
+const playAgain = document.querySelector(".play-again-btn");
+const moveCounter = document.querySelector(".moves-counter");
+const star = document.getElementById("star-rating").querySelectorAll(".star");
 
+// Shuffle function From http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
-
-let modal = document.getElementById("popup1")
-
-function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
-
-    this.classList.add('flip');
-
-    if (!hasFlippedCard) {
-        // first click
-        hasFlippedCard = true;
-        firstCard = this;
-
-        return;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
 
-    // second click
-    secondCard = this;
-
-    checkForMatch();
+    return array;
 }
 
-function checkForMatch() {
-    let isMatch = firstCard.dataset.image === secondCard.dataset.image;
-
-    isMatch ? disableCards() : unflipCards();
+function startGame() {
+    const shuffledDeck = shuffle(cards);
+    for (let i = 0; i < shuffledDeck.length; i++) {
+        const liTag = document.createElement('LI');
+        liTag.classList.add('card')
+        const addImage = document.createElement("IMG");
+        liTag.appendChild(addImage);
+        addImage.setAttribute("src", "img/" + shuffledDeck[i]);
+        addImage.setAttribute("alt", "wario");
+        cardsContainer.appendChild(liTag);
+    }
 }
+startGame();
 
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-
-    resetBoard();
+function removeCard() {
+    while (cardsContainer.hasChildNodes()) {
+        cardsContainer.removeChild(cardsContainer.firstChild);
+    }
 }
+function timer() {
+    time = setInterval(function () {
+        seconds++;
+        if (seconds === 60) {
+            minutes++;
+            seconds = 0;
+        }
+        timeCounter.innerHTML = "<i class = 'fa fa-hourglass-start'></i>" + "Timer:" + minutes + "Mins" + seconds + "Secs";
 
-function unflipCards() {
-    lockBoard = true;
-
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-
-        resetBoard();
     }, 1500);
+
+}
+function stopTime() {
+    clearInterval(time);
+}
+function resetEverything() {
+    stopTime();
+    timeStart = false;
+    seconds = 0;
+    minutes = 0;
+    timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + "Timer:00:00";
+
+    star[1].firstElementChild.classList.add("fa-star");
+    star[2].firstElementChild.classList.add("fa-star");
+    starCount = 3;
+    movees = 0;
+    moveCounter.innerHTML = 0;
+
+    matchedCards = [];
+    currentOpenedCards = [];
+    removeCard();
+    startGame();
+
+}
+function movesCounter() {
+    moveCounter.innerHTML++;
+    moves++;
+}
+function starRating() {
+    if (moves === 15) {
+        star[2].firstElementChild.classList.remove("fa-star");
+        starCount--;
+    }
+    if (moves === 19) {
+        star[1].firstElementChild.classList.remove("fa-star");
+        starCount--;
+    }
 }
 
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
+function compareTwo() {
+    if (currentOpenedCards.length === 2) {
+        document.body.style.pointerEvents = "none";
+
+    }
+    if (currentOpenedCards.length === 2 && currentOpenedCards[0].src === currentOpenedCards[1].src) {
+        match();
+    } else if (currentOpenedCards.length === 2 && currentOpenedCards[0].src != currentOpenedCards[1].src) {
+        noMatch();
+    }
 }
 
-(function shuffle() {
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * 12);
-        card.style.order = randomPos;
-    });
-})();
+function match() {
+    setTimeout(function () {
+        currentOpenedCards[0].parentElement.classList.add("match");
+        currentOpenedCards[1].parentElement.classList.add("match");
+        matchedCards.push(...currentOpenedCards);
+        document.body.style.pointerEvents = "auto";
+        winGame();
+        currentOpenedCards = [];
+    }, 600);
+    movesCounter();
+    starRating();
+}
+function noMatch() {
+    setTimeout(function () {
+        currentOpenedCards[0].parentElement.classList.remove("flip");
+        currentOpenedCards[1].parentElement.classList.remove("flip");
+        document.body.style.pointerEvents = "auto";
+        currentOpenedCards = [];
+    }, 700);
+    movesCounter();
+    starRating();
+}
+function AddStats() {
+    const stats = document.querySelector(".modal-content");
+    for (let i = 1; i <= 3; i++) {
+        const statsElement = document.createElement("p");
+        statsElement.classList.add("stats");
+        stats.appendChild(statsElement);
+    }
+    let p = stats.querySelectorAll("p.stats");
+    p[0].innerHTML = "Time to complete:" + minutes + "Minutes and" + seconds + "Seconds";
+    p[1].innerHTML = "Moves Taken:" + moves;
+    p[2].innerHTML = "Your Star Rating is:" + starCount + "out of 3";
+}
+function displayModal() {
+    const modalClose = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+    modalClose.onclick = function () {
+        modal.style.display = "none";
+    };
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
 
+    };
+}
+function winGame() {
+    if (matchedCards.length === 12) {
+        stopTime();
+        AddStats();
+        displayModal();
 
+    }
+}
+cardsContainer.addEventListener("click", function (evt) {
+    if (evt.target.nodeName === "LI") {
+        console.log(evt.target.nodeName + "Was clicked");
+        if (timeStart === false) {
+            timeStart = true;
+            timer();
+        }
+        flipCard();
+    }
+    function flipCard() {
+        evt.target.classList.add("flip");
+        addToOpened();
+    }
+    function addToOpened() {
+        if (currentOpenedCards.length === 0 || currentOpenedCards.length === 1) {
+            currentOpenedCards.push(evt.target.firstElementChild);
+        }
+        compareTwo();
 
-
-cards.forEach(card => card.addEventListener('click', flipCard));
+    }
+});
+reset.addEventListener('click', resetEverything);
+playAgain.addEventListener('click', function () {
+    modal.style.display = "none";
+    resetEverything();
+});
+ 
+    
